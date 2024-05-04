074de0a9985c269d88d2d06f9d8620be8a1819b3
Mahamudur Rahaman <mahamudur@Mahamudurs-MacBook-Air.local>
2024-05-04 20:11:43 +0600

    first commit4

--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
+import Loading from "@/components/Loading";
 import RecipeList from "@/components/RecipeList";
 import CategoryListSection from "@/components/landing/CategoryListSection";
 import HeroSection from "@/components/landing/HeroSection";
+import { Suspense } from "react";
 
 export default function Home() {
     return (
@@ -8,8 +10,10 @@ export default function Home() {
             <HeroSection />
             <section className='container py-8'>
                 <div className='grid grid-cols-12 py-4'>
-                    <CategoryListSection />
-                    <RecipeList />
+                    <Suspense key={"khana"} fallback={<Loading />}>
+                        <CategoryListSection />
+                        <RecipeList />
+                    </Suspense>
                 </div>
             </section>
         </>
--- components/details/RecipeDetails.jsx
+++ components/details/RecipeDetails.jsx
@@ -3,6 +3,7 @@ import { timeFormating } from "@/utils/data-util";
 import Image from "next/image";
 import Link from "next/link";
 import ActionFavourite from "./ActionFavourite";
+import SocialShare from "./SocialShare";
 
 export default async function RecipeDetails({ recipe }) {
     const { base64 } = await getBlurData(recipe?.thumbnail);
@@ -131,31 +132,7 @@ export default async function RecipeDetails({ recipe }) {
                     <div className='flex gap-4 justify-end'>
                         <ActionFavourite recipeId={recipe?.id} />
 
-                        <div className='flex gap-2 text-gray-600 cursor-pointer hover:text-[#0E79F6]'>
-                            <svg
-                                xmlns='http://www.w3.org/2000/svg'
-                                width='24'
-                                height='24'
-                                viewBox='0 0 24 24'
-                                fill='none'
-                                stroke='currentColor'
-                                strokeWidth='2'
-                                strokeLinecap='round'
-                                strokeLinejoin='round'
-                            >
-                                <path
-                                    stroke='none'
-                                    d='M0 0h24v24H0z'
-                                    fill='none'
-                                />
-                                <path d='M6 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0' />
-                                <path d='M18 6m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0' />
-                                <path d='M18 18m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0' />
-                                <path d='M8.7 10.7l6.6 -3.4' />
-                                <path d='M8.7 13.3l6.6 3.4' />
-                            </svg>
-                            <span>Share</span>
-                        </div>
+                        <SocialShare title={recipe?.name} />
                     </div>
                 </div>
             </div>

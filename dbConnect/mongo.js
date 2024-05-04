45252e0b72820b833375d327d6b8bb69fe7825ad
Mahamudur Rahaman <mahamudur@Mahamudurs-MacBook-Air.local>
2024-05-04 20:43:45 +0600

    Update 7

--- dbConnect/mongo.js
+++ dbConnect/mongo.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export async function mongoDBConnect() {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect(process.env.MONGO_URL);
         console.log("Mongo Database Connected");
         return conn;
     } catch (err) {
